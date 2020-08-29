e574de033a9f7fdc7adc3c412b5b248ac05d5b76
moonfrank <franconicolas2007@hotmail.com>
2020-08-29 12:43:59 -0300

    Lab 5.1 terminado

---
+++ WebSite/Login.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnIngresar_Click(object sender, EventArgs e)
+    {
+        if (txtUsuario.Text.ToLower() == "admin" && txtClave.Text.ToLower() == "admin")
+            Page.Response.Write("Ingreso OK");
+        else Page.Response.Write("Usuario y/o contraseña incorrectos");
+    }
+
+    protected void lnkRecordarClave_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Default.aspx?msj=Qué macana, che...");
+    }
+}
